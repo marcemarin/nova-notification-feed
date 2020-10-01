d64053f670d7b8058b6a144cd769fe89c6b4be1c
Marce Marin <marcelo.marin.salvagiot@gmail.com>
2020-10-01 11:19:02 -0300

    color transparent

--- resources/js/components/NotificationsPanel.vue
+++ resources/js/components/NotificationsPanel.vue
@@ -1,99 +1,102 @@
 <template>
-  <div class="text-white notifications-panel">
-    <div class="border-b border-80">
-      <div class="text-center px-6" id="notifications-panel-close" @click="toggleNotificationsPanel">
-        Close
-      </div>
+    <div class="text-white notifications-panel">
+        <div class="border-b border-80">
+            <div
+                class="text-center px-6"
+                id="notifications-panel-close"
+                @click="toggleNotificationsPanel"
+            >
+                Close
+            </div>
+        </div>
+        <div class="px-4 border-b border-80 overflow-y-scroll h-full">
+            <div v-for="notification in notifications">
+                <notification-message :notification="notification"></notification-message>
+            </div>
+            <infinite-loading @infinite="getNotifications"></infinite-loading>
+        </div>
     </div>
-    <div class="px-4 border-b border-80 overflow-y-scroll h-full">
-      <div v-for="notification in notifications">
-        <notification-message :notification="notification"></notification-message>
-      </div>
-      <infinite-loading @infinite="getNotifications"></infinite-loading>
-    </div>
-  </div>
 </template>
 
 <script>
-  import InfiniteLoading from 'vue-infinite-loading'
+import InfiniteLoading from 'vue-infinite-loading';
 
-  export default {
+export default {
     name: 'NotificationsPanel',
-    props: [
-      'broadcastOn'
-    ],
+    props: ['broadcastOn'],
     components: {
-      InfiniteLoading,
+        InfiniteLoading,
     },
-    data () {
-      return {
-        notifications: [],
-        interval: null,
-        currentPage: 0
-      }
+    data() {
+        return {
+            notifications: [],
+            interval: null,
+            currentPage: 0,
+        };
     },
     methods: {
-      toggleNotificationsPanel: function () {
-        this.$emit('toggleNotificationsPanel')
-      },
-      getNotifications: function ($state) {
-        this.currentPage += 1
-        axios.get('/nova-vendor/nova-notifications/notifications?page=' + this.currentPage).then(response => {
-          this.$emit('showUnreadNotificationCount', response.data.meta.unread_count)
-          if (response.data.data.length) {
-            response.data.data.forEach(i => {
-              this.notifications.push(i)
-            })
-            if ($state !== undefined) {
-              $state.loaded()
-            }
-          } else {
-            if($state !== undefined) {
-              $state.complete()
-            }
-          }
-          // Assign current page just for redundancy's sake
-          this.currentPage = response.data.meta.current_page
-        })
-      },
-      listenForNotifications () {
-        window.userPrivateChannel
-          .notification((notification) => {
-            // Increment the unread count
-            this.$emit('incrementUnreadCount')
-            // Add the notification to the top
-            this.notifications.unshift(notification)
-            // Show a toast
-            this.$toasted.show(notification.data.message, {type: notification.data.level})
-          })
-      }
+        toggleNotificationsPanel: function() {
+            this.$emit('toggleNotificationsPanel');
+        },
+        getNotifications: function($state) {
+            this.currentPage += 1;
+            axios
+                .get('/nova-vendor/nova-notifications/notifications?page=' + this.currentPage)
+                .then(response => {
+                    this.$emit('showUnreadNotificationCount', response.data.meta.unread_count);
+                    if (response.data.data.length) {
+                        response.data.data.forEach(i => {
+                            this.notifications.push(i);
+                        });
+                        if ($state !== undefined) {
+                            $state.loaded();
+                        }
+                    } else {
+                        if ($state !== undefined) {
+                            $state.complete();
+                        }
+                    }
+                    // Assign current page just for redundancy's sake
+                    this.currentPage = response.data.meta.current_page;
+                });
+        },
+        listenForNotifications() {
+            window.userPrivateChannel.notification(notification => {
+                // Increment the unread count
+                this.$emit('incrementUnreadCount');
+                // Add the notification to the top
+                this.notifications.unshift(notification);
+                // Show a toast
+                this.$toasted.show(notification.data.message, { type: notification.data.level });
+            });
+        },
     },
-    created () {
-      this.getNotifications()
-      this.listenForNotifications()
-    }
-  }
+    created() {
+        this.getNotifications();
+        this.listenForNotifications();
+    },
+};
 </script>
 
 <style scoped>
-  .notifications-panel {
+.notifications-panel {
     z-index: 999;
     position: fixed !important;
     right: 0;
     top: 0;
     width: 340px;
     height: 100%;
-    background-color: #536170;
+    background-color: transparent;
     padding-bottom: 70px;
-  }
+}
 
-  #notifications-panel-close {
+#notifications-panel-close {
     height: 60px;
     line-height: 60px;
     cursor: pointer;
-  }
+}
 
-  #notifications-panel-close:hover {
-    background-color: #252D37;
-  }
+#notifications-panel-close:hover {
+    background-color: #252d37;
+}
 </style>
